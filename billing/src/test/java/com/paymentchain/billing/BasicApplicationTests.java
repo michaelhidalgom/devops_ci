27214a715b080aa9f96aef36c229a22b714cf4c8
cmbsystem <imbsystem@imbsystem.com>
2021-08-17 12:49:55 -0400

    modifico test.java

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -6,11 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	//se inicio un cambibo
 	}
 
 }
